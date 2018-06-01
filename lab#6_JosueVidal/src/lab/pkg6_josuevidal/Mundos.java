b29ec8dd63563c59018f2c5d7e26bc5322eaabce
vidalinho <josuevfp@gmail.com>
2018-06-01 14:36:29 -0600

    creacion de clase universo

--- lab#6_JosueVidal/src/lab/pkg6_josuevidal/Mundos.java
+++ lab#6_JosueVidal/src/lab/pkg6_josuevidal/Mundos.java
@@ -5,10 +5,47 @@
  */
 package lab.pkg6_josuevidal;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author josue
  */
 public class Mundos {
+    private String nombre;
+    private int peso;
+    ArrayList <Criaturas> creature=new ArrayList();
+
+    public Mundos() {
+    }
+
+    public Mundos(String nombre, int peso) {
+        this.nombre = nombre;
+        this.peso = peso;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public ArrayList<Criaturas> getCreature() {
+        return creature;
+    }
+
+    public void setCreature(ArrayList<Criaturas> creature) {
+        this.creature = creature;
+    }
     
 }
--- lab#6_JosueVidal/src/lab/pkg6_josuevidal/Universos.java
+++ lab#6_JosueVidal/src/lab/pkg6_josuevidal/Universos.java
@@ -10,5 +10,46 @@ package lab.pkg6_josuevidal;
  * @author josue
  */
 public class Universos {
-    
+   private int codigo;
+   private int edad;
+   private int peso;
+
+    public Universos() {
+    }
+
+    public Universos(int codigo, int edad, int peso) {
+        this.codigo = codigo;
+        this.edad = edad;
+        this.peso = peso;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    @Override
+    public String toString() {
+        return "Universos{" + "codigo=" + codigo + ", edad=" + edad + ", peso=" + peso + '}';
+    }
+   
 }
